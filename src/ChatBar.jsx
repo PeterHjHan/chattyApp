d399adcac3d2bf7b25eabeccd738e0c6cccd8e22
= <peterhjhan@gmail.com>
2018-11-01 04:35:50 +0000

    usernanme Changed in client side only

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { EventEmitter } from 'events';
 
 function randomId() {
   return '_' + Math.
@@ -17,7 +18,33 @@ class ChatBar extends Component{
     this.getChatText = this.getChatText.bind(this);
     this.onChatSubmit = this.onChatSubmit.bind(this);
     this.getUserName = this.getUserName.bind(this); 
+    this.onUserNameSubmit = this.onUserNameSubmit.bind(this);
   };
+
+
+  //TODO: Bug where typing enter creates new same chat even though value shows nothing;
+  //TODO: Error handling
+
+
+
+
+  getUserName(event) {
+    if(event.target.value.length === 0) {
+      this.setState({username: "Mysterious"})
+    } else {
+      this.setState({username: event.target.value})
+    }
+  }
+
+  onUserNameSubmit(event) {
+    if(event.keyCode == 13){
+      if(event.target.value.length === 0) {
+        this.setState({username: "Mysterious"})
+      } else {
+        this.setState({username: event.target.value})
+      }
+    }
+  }
 
   getChatText(event) {
     if(event.target.value.length === 0) {
@@ -26,26 +53,12 @@ class ChatBar extends Component{
       })
     } else {
       this.setState({
-        id: randomId(),
-        username: this.props.currentUser.name,
+        id: this.props,
+        username: this.state.username,
         content: event.target.value,
       });
     }
   }
-
-  getUserName(event) {
-    this.setState({username: event.target.value})
-  }
-
-  onUserNameSubmit(event) {
-    if(event.keyCode == 13){
-      this.setState({username: event.target.value})
-    }
-  }
-
-
-  //TODO: Bug where typing enter creates new same chat even though value shows nothing;
-  //TODO: Error handling
 
   onChatSubmit(event) {
     if(event.keyCode == 13){
@@ -67,14 +80,11 @@ class ChatBar extends Component{
   render() {
 
     return (
-
-      <form onSubmit ={this.onSubmit}>
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder={this.state.user} onChange ={this.getUserName} onKeyUp = {this.onUserNameSubmit} name="username"/>
+        <input className="chatbar-username" placeholder={this.state.username} onChange ={this.getUserName} onKeyUp = {this.onUserNameSubmit} name="username"/>
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" onChange={this.getChatText}
         onKeyUp ={this.onChatSubmit} name="chatContent"/>
       </footer>
-      </form>
     )
   }
 }
